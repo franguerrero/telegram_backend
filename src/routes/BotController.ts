646ee2512e0b64a4a93df6785f7ec62e479e22c2
franguerrero <franguerrero@hotmail.com>
2024-01-17 17:57:31 +0100

    modifico a pregunta a enviar al canal

--- src/routes/BotController.ts
+++ src/routes/BotController.ts
@@ -29,8 +29,23 @@ class BotController {
       if (!process.env.CANALADMINISTRATIVOAND) {
         throw new Error("CANALADMINISTRATIVOAND is not defined in .env file");
       }
-      // Enviar el mensaje de bienvenida al usuario
-      await this.bot.sendMessage(process.env.CANALADMINISTRATIVOAND, welcomeMessage);
+     
+      const question = await QuizService.getRandomQuestion();
+      if (question) {
+        const questionText = question.text;
+        const options = question.options; // Asegúrate de que esto es un array de opciones
+        const correctOptionIndex = question.answer; // El índice de la opción correcta
+  
+        await this.bot.sendPoll(message.chat.id, questionText, options, {
+          is_anonymous: true,
+          type: 'quiz',
+          correct_option_id: correctOptionIndex,
+          explanation: 'question.explanation', // Explicación que se mostrará después de responder
+          explanation_parse_mode: 'Markdown' // O 'HTML', si tu explicación tiene formato
+        });
+      } else {
+        await this.bot.sendMessage(process.env.CANALADMINISTRATIVOAND, "Lo siento, no tengo preguntas disponibles en este momento.");
+      }
     } catch (error) {
       console.error("Error al enviar el mensaje de bienvenida:", error);
     }
